c3febf63c01ebe3c48e5b52dc92eb82ae9995a82
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-04 16:55:10 +0200

    feat: add construct function to Cat class

--- Model/Cat.php
+++ Model/Cat.php
@@ -1,1 +1,12 @@
 <?php
+
+class Cat extends Product
+{
+    public $animal;
+
+    public function __construct(string $_name, string  $_brend, string  $_categori, string  $_description, int $_price, string  $_animal)
+    {
+        parent::__construct($_name, $_brend, $_categori, $_description, $_price);
+        $this->animal = $_animal;
+    }
+}
