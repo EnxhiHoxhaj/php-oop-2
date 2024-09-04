a2cfbb687ab1b9b65a54fcf89571180696a6d121
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-04 16:54:01 +0200

    faet: add costruct function to Dog class

--- Model/dog.php
+++ Model/dog.php
@@ -3,4 +3,10 @@
 class Dog extends Product
 {
     public $animal;
+
+    public function __construct(string $_name, string  $_brend, string  $_categori, string  $_description, int $_price, string  $_animal)
+    {
+        parent::__construct($_name, $_brend, $_categori, $_description, $_price);
+        $this->animal = $_animal;
+    }
 }
