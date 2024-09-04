165ce1d703d7c4d7a50924e9e2e5b23c913b35d8
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-04 16:26:47 +0200

    feat: add css file

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+// includo gli altri file nel mio index
+require_once __DIR__ . '/Model/Shop.php';
+require_once __DIR__ . '/Model/Cat.php';
+require_once __DIR__ . '/Model/Dog.php';
+require_once __DIR__ . '/Model/Db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Shop</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+    L’e-commerce vende prodotti per animali.
+    - I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+    - I prodotti saranno oltre al cibo, anche giochi, cucce, etc
+    Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo
+    che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+
+</body>
+
+</html>
